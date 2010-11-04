da1978bbba994573848c448de9cec1bb169a48b8
mh <mh@immerda.ch>
2010-11-04 14:16:55 +0100

    init script is now part of the rpm

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,12 +1,5 @@
 class tinc::centos inherits tinc::base {
 
-  file{'/etc/init.d/tinc':
-    source => "puppet:///modules/tinc/${operatingsystem}/tinc.init",
-    require => Package['tinc'],
-    before => Service['tinc'],
-    owner => root, group => 0, mode => 0755;
-  }
-
   file{'/etc/sysconfig/tinc':
     source => [ "puppet:///modules/site-tinc/CentOS/${fqdn}/tinc.sysconfig",
                 "puppet:///modules/site-tinc/tinc.sysconfig",
