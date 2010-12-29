e41cf10088e9b9cc85ac6f4eab1ad42ad753502f
mh <mh@immerda.ch>
2010-12-29 22:53:40 +0100

    some more magic to lookup the ip address for the bridged interface

--- manifests/vpn_net.pp
+++ manifests/vpn_net.pp
@@ -87,8 +87,15 @@ define tinc::vpn_net(
     }
 
     if $tinc_internal_ip == 'absent' {
-      $tinc_orig_ifaddr = "ipaddress_${tinc_internal_interface}"
-      $real_tinc_internal_ip = inline_template("<%= scope.lookupvar(tinc_orig_ifaddr) %>")
+      $tinc_br_ifaddr = "ipaddress_${real_tinc_bridge_interface}"
+      $tinc_br_ip = inline_template("<%= scope.lookupvar(tinc_br_ifaddr) %>")
+      case $tinc_br_ip {
+        '',undef: {
+          $tinc_orig_ifaddr = "ipaddress_${tinc_internal_interface}"
+          $real_tinc_internal_ip = inline_template("<%= scope.lookupvar(tinc_orig_ifaddr) %>")
+        }
+        default: { $real_tinc_internal_ip = $tinc_br_ip }
+      }
     } else {
       $real_tinc_internal_ip = $tinc_internal_ip
     }
